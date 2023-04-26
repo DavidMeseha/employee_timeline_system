b453ad30b83318d1f95cdc9fe26c690c3768abf6
DavidMeseha <111684897+DavidMeseha@users.noreply.github.com>
2023-04-26 15:12:43 +0200

    date process

--- src/context/DisplayManagerProvider.js
+++ src/context/DisplayManagerProvider.js
@@ -33,30 +33,38 @@ export const DisplayManagerProvider = ({ children }) => {
     }, [])
 
     useEffect(() => {
-        if (format === 'weekly') {
-            let endDate = date
-            let endMonth = endDate.toLocaleDateString('en', { month: 'long' })
-            let endYear = endDate.toLocaleDateString('en', { year: 'numeric' })
-            let endDay = endDate.getDate()
+        const switchDateDisplayFormat = () => {
+            if (format === 'weekly') {
+                let endDate = date
+                let endMonth = endDate.toLocaleDateString('en', { month: 'long' })
+                let endYear = endDate.toLocaleDateString('en', { year: 'numeric' })
+                let endDay = endDate.getDate()
 
-            let startDate = new Date(endDate.setDate(endDate.getDate() - 7))
-            let startMonth = startDate.toLocaleDateString('en', { month: 'long' })
-            let startYear = startDate.toLocaleDateString('en', { year: 'numeric' })
-            let startDay = startDate.getDate()
+                let startDate = new Date(endDate.setDate(endDate.getDate() - 7))
+                let startMonth = startDate.toLocaleDateString('en', { month: 'long' })
+                let startYear = startDate.toLocaleDateString('en', { year: 'numeric' })
+                let startDay = startDate.getDate()
 
-            let displayDate = ''
-            if (startMonth === endMonth) displayDate = `${startDay} - ${endDay} ${endMonth} ${endYear}`
-            if (startMonth !== endMonth) {
-                if (startYear === endYear) displayDate = `${startDay} ${startMonth} - ${endDay} ${endMonth} ${endYear}`
-                else displayDate = `${startDay} ${startMonth} ${startYear} - ${endDay} ${endMonth} ${endYear}`
+                let displayDate = ''
+                if (startMonth === endMonth) displayDate = `${startDay} - ${endDay} ${endMonth} ${endYear}`
+                if (startMonth !== endMonth) {
+                    if (startYear === endYear) displayDate = `${startDay} ${startMonth} - ${endDay} ${endMonth} ${endYear}`
+                    else displayDate = `${startDay} ${startMonth} ${startYear} - ${endDay} ${endMonth} ${endYear}`
 
+                }
+
+                setSelectedDate(displayDate)
             }
 
-            setSelectedDate(displayDate)
+            if (format === 'daily') setSelectedDate(new Date().toLocaleDateString('en', { day: 'numeric', month: 'long', weekday: 'long', year: 'numeric' }))
         }
 
-        if (format === 'daily') setSelectedDate(new Date().toLocaleDateString('en', { day: 'numeric', month: 'long', weekday: 'long', year: 'numeric' }))
+        switchDateDisplayFormat()
     }, [format])
+
+    const nextDate = () =>{
+        
+    }
 
     return (
         <DisplayManagerContext.Provider value={{
