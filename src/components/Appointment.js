d4a8e7f9676c7b7eacda8c26e50e8b05b0daa5e8
DavidMeseha <111684897+DavidMeseha@users.noreply.github.com>
2023-04-28 09:13:54 +0300

    fix loading overlab

--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -1,6 +1,18 @@
-const Appointment = () => {
-    return(
-        <></>
+const Appointment = ({ appointment }) => {
+    let startHours = parseInt(appointment.start.split(':')[0])
+    let startMinutes = parseInt(appointment.start.split(':')[1])
+    let startTotalMinutes = (startHours * 60) + startMinutes
+    console.log(startHours, startMinutes, startTotalMinutes)
+
+    let endHours = parseInt(appointment.end.split(':')[0])
+    let endMinutes = parseInt(appointment.end.split(':')[1])
+    let endTotalMinutes = (endHours * 60) + endMinutes
+
+    let appointmentStart = startTotalMinutes + ((startTotalMinutes / 15) * 0.8) - 2
+    let appointmentEnd = (endTotalMinutes - startTotalMinutes) + (((endTotalMinutes - startTotalMinutes) / 15) * 0.8)
+
+    return (
+        <div style={{ top: appointmentStart, height: appointmentEnd }} className="appointment"></div>
     )
 };
 
--- src/layouts/DailyTimeline.js
+++ src/layouts/DailyTimeline.js
@@ -1,3 +1,4 @@
+import Appointment from "@/components/Appointment";
 import TimeLines from "@/components/TimeLines";
 import useDisplayManger from "@/Hooks/useDataManger";
 import { useEffect, useState } from "react";
@@ -46,21 +47,7 @@ const DailyTimeLine = () => {
                                                 <td>
                                                     {employee.appointments?.map((appointment) => {
                                                         if (new Date(appointment.date).getDate() !== date.getDate()) return
-                                                        let startHours = parseInt(appointment.start.split(':')[0])
-                                                        let startMinutes = parseInt(appointment.start.split(':')[1])
-                                                        let startTotalMinutes = (startHours * 60) + startMinutes
-                                                        console.log(startHours, startMinutes, startTotalMinutes)
-
-                                                        let endHours = parseInt(appointment.end.split(':')[0])
-                                                        let endMinutes = parseInt(appointment.end.split(':')[1])
-                                                        let endTotalMinutes = (endHours * 60) + endMinutes
-
-                                                        let appointmentStart = startTotalMinutes + ((startTotalMinutes / 15) * 0.8) - 2
-                                                        let appointmentEnd = (endTotalMinutes - startTotalMinutes) + (((endTotalMinutes - startTotalMinutes) / 15) * 0.8)
-
-                                                        return (
-                                                            <div style={{ top: appointmentStart, height: appointmentEnd }} className="appointment"></div>
-                                                        )
+                                                        <Appointment appointment={appointment} />
                                                     })}
                                                 </td>
                                             )
