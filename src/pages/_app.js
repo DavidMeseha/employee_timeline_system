9856ced295cba9e21c5498ace8877754d70c3b8b
DavidMeseha <111684897+DavidMeseha@users.noreply.github.com>
2023-05-05 10:29:58 +0300

    drags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import '@/styles/optionsbar.css'
 import '@/styles/timeline.css'
 import '@/styles/dailytimeline.css'
-import '@/styles/Confirmedit.css'
+import '@/styles/confirmedit.css'
 import { DisplayManagerProvider } from '@/context/DisplayManagerProvider'
 import { EmployeesProvider } from '@/context/EmployeesProvider'
 
