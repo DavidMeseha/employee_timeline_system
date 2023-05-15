6c2b744720b37b10add4fe4b0ccc9e9b2a2c9670
DavidMeseha <111684897+DavidMeseha@users.noreply.github.com>
2023-05-15 22:37:18 +0300

    fix views

--- src/components/EmployeesDropdown.js
+++ src/components/EmployeesDropdown.js
@@ -51,7 +51,7 @@ const EmployeesDropdown = ({ employees, selected, setSelected, format }) => {
         if (!allCheckboxRef.current) return
         allCheckboxRef.current.checked = true
         selectAll()
-    }, [format])
+    }, [format, selected])
 
     useEffect(() => {
         if (!checkboxesRef.current[0]) return;
